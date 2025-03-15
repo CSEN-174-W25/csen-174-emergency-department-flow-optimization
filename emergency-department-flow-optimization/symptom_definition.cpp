bb1aaacd8c11f2daac92e693b4d6efafe51f21e4
Sumeet-U <116345857+Sumeet-U@users.noreply.github.com>
2025-03-14 22:51:34 -0700

    Cleaned Up Duplicate Files

--- emergency-department-flow-optimization/symptom_definition.cpp
+++
@@ -1,6 +0,0 @@
-#include "symptom_definition.h"
-
-const SymptomDefinition::PresetSymptom* SymptomDefinition::findSymptomById(const std::string& id){ //Finish this function
-    return nullptr;
-}
-
--- emergency-department-flow-optimization/tests.cpp
+++
@@ -1,7 +0,0 @@
-#include "tests.h"
-
-tests::tests() {}
-
-void tests::addPatient(){
-
-}
